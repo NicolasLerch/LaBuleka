2586b8b551f5ed410d606325d26ff8e32ff89580
NicoLerch <121203780+NicolasLerch@users.noreply.github.com>
2023-11-28 22:39:00 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,35 @@
+
+
+//Next and prev controls
+
+const plusSlides = (n) => {
+    showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+const showSlides = (n) => {
+    let i;
+    let slides = document.getElementsByClassName("slide");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length){
+        slideIndex = 1;
+    }
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+    for (i=0; i < slides.length; i++){
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+}
+
+let slideIndex = 1;
+showSildes(slideIndex);
