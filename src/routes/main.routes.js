b218751df216b4da218e689e787c4e93927ebd4c
NicolasLerch <lerchnicolas@hotmail.com>
2024-05-18 13:57:38 -0300

    listo el carrito individual para cada usuario

--- src/routes/main.routes.js
+++ src/routes/main.routes.js
@@ -9,18 +9,21 @@ const userController = require('../controllers/userController')
 router.get('/', mainController.viewIndex);
 router.get('/local', mainController.viewLocal);
 router.get('/tienda', mainController.viewTienda);
-router.get('/cart', cartController.getCart);
-router.post('/cart/:id', cartController.addToCart);
+
 
 router.get('/login', mainController.login)
 router.get('/register', mainController.register)
 router.post('/register', userController.create)
 
+router.get('/cart', cartController.getCart);
+router.post('/cart/:id', cartController.addToCart);
 // limpiar el carrito completo
 router.delete('/cart', cartController.emptyCart)
 
 // borrar un elemento del carrito
 router.delete('/cart/:id', cartController.deleteFromCart)
+
+router.get('/checksession', mainController.check)
 
 
 
