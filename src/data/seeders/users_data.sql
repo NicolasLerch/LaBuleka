7deed7fffce46ad2259c152edd33a68b115c97b9
NicolasLerch <lerchnicolas@hotmail.com>
2024-07-27 13:52:53 -0300

    DB schema and populate scripts added

---
+++ src/data/seeders/users_data.sql
@@ -0,0 +1,7 @@
+INSERT INTO users (id, name, last_name, email, password, rol) VALUES
+(4, 'Pepe', 'Grillo', 'pepe@prueba.com', '$2a$10$mx6kXbDeRSG0K7zxJn0bDObfsFC0mbvNz9G09Ncs7yw7B1TiZv1ge', 'client'),
+(5, 'Flor', 'Lerch', 'florlerch@mail.com', '$2a$10$DeGjTACKttPEbV3E6MfT3.oEacOJaMJW9Ms3QXdms7g.uqQC1mJzu', 'client'),
+(7, 'Rocket', 'Rocketon', 'Rocketelperro@mail.com', '$2a$10$3Cha6t/c5EmamE/1W5l6TuW4SWRB9p5E4ySY/3ADgivumN1JtDPIy', 'client'),
+(8, 'Rayitas', 'La gata', 'rayitas@mail.com', '$2a$10$T8oNDKyS/CcroAESJKTN.eIGvkKp5PYqYCQ2kceTph1irndsXopNS', 'client'),
+(14, 'Storm', 'Trooper', 'stormtrooper@mail.com', '$2a$10$YmGNuhZYwWXraYfEPDuvO.tCoiMZf/MQrb5sDDI7XREVe5HisZEwK', 'client'),
+(15, 'Leia', 'Organa', 'leia@starwars.com', '$2a$10$16a9vjkK2LXMgwq4swmfy.yHpSIlSsbFyciw1SxjicGaoec.6ybeG', 'client');
